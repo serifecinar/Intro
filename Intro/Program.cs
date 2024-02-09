11521111eb7856bf2066ef9c9ac51ad999f94b10
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-02-09 13:09:21 +0300

    Proje dosyası ekle.

---
+++ Intro/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string message1 = "krediler";
+int term = 12;
+double amount = 100000.5;
+bool isAuthenticated = true;
+
+Console.WriteLine(message1);
