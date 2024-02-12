0239132a327c9fbbef10341eac7082092641d538
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-02-12 15:37:52 +0300

    ++

--- Intro/Business/CourseManager.cs
+++ Intro/Business/CourseManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Intro.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace Intro.Business;
 
-internal class CourseManager
+public class CourseManager
 {
+    Course[] courses = new Course[3];
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "c#";
+        course1.Description = ".8 ve vs..";
+        course1.Price = 0;
 
+        Course course2 = new Course();
+        course2.Id = 2;
+        course2.Name = "java 17...";
+        course2.Description = ".8 ve vs..";
+        course2.Price = 10;
+
+        Course course3 = new Course();
+        course3.Id = 3;
+        course3.Name = "phyton";
+        course3.Description = "phyton 3.12...";
+        course3.Price = 20;
+
+        //Course[] courses = { course1, course2, course3 };
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+    }
+    public Course[] GetAll()
+    {
+        //veri kaynağından çekilir
+        //Console.WriteLine("Kurslar listelendi");
+
+        return courses;
+    }
 }
--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
 using Intro.Entities;
 
 Console.WriteLine("Hello, World!");
@@ -42,29 +43,37 @@ for (int i = 0; i < loans.Length;i++)
     Console.WriteLine(loans[i]);
 }
 
-Course course1= new Course();
-course1.Id= 1;
-course1.Name = "c#";
-course1.Description = ".8 ve vs..";
-course1.Price = 0;
+//Course course1= new Course();
+//course1.Id= 1;
+//course1.Name = "c#";
+//course1.Description = ".8 ve vs..";
+//course1.Price = 0;
 
-Course course2 = new Course();
-course2.Id = 2;
-course2.Name = "java 17...";
-course2.Description = ".8 ve vs..";
-course2.Price =10;
+//Course course2 = new Course();
+//course2.Id = 2;
+//course2.Name = "java 17...";
+//course2.Description = ".8 ve vs..";
+//course2.Price =10;
 
-Course course3 = new Course();
-course3.Id = 3;
-course3.Name = "phyton";
-course3.Description = "phyton 3.12...";
-course3.Price =20;
+//Course course3 = new Course();
+//course3.Id = 3;
+//course3.Name = "phyton";
+//course3.Description = "phyton 3.12...";
+//course3.Price =20;
 
-Course[] courses = { course1, course2, course3 };
+//Course[] courses = { course1, course2, course3 };
 
-for (int i = 0; i < courses.Length; i++)
+//for (int i = 0; i < courses.Length; i++)
+//{
+//    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+//}
+
+CourseManager courseManager = new CourseManager();
+Course[] courses2=courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
 {
-    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
 }
 
 Console.WriteLine("Buton--> Kod Bitti");
