0834931fad249927bd76ff471fb76003ff08d450
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-02-12 14:11:38 +0300

    ++

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -3,10 +3,13 @@ Console.WriteLine("Hello, World!");
 string message1 = "krediler";
 int term = 12;
 double amount = 100000.5;
+
+//variable - camelCase
 bool isAuthenticated = true;
 
 Console.WriteLine(message1);
 
+//condition
 if (isAuthenticated)
 {
     Console.WriteLine("Buton--> Hoşgeldin Şerife");
@@ -15,4 +18,26 @@ else
 {
     Console.WriteLine("Buton--> Sisteme giriş yap");
 }
+
+//string kredi1 = "Kredi 1";
+//string kredi2 = "Kredi 2";
+//string kredi3 = "Kredi 3";
+//string kredi4= "Kredi 4";
+//string kredi5= "Kredi 5";
+
+//Console.WriteLine(kredi1);
+//Console.WriteLine(kredi2);
+//Console.WriteLine(kredi3);
+//Console.WriteLine(kredi4);
+//Console.WriteLine(kredi5);
+
+string[] loans = { "KREDİ1", "KREDİ2", "KREDİ3", "KREDİ4", "KREDİ5", "KREDİ6" }; //db den gelecek
+//Console.WriteLine(loans[2]);
+
+        //start   condition increment
+for (int i = 0; i < loans.Length;i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
 Console.WriteLine("Buton--> Kod Bitti");
