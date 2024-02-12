519d5abe03d48a5213d7e9e5d35f475f7c4268bf
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-02-12 14:41:53 +0300

    ++

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Intro.Entities;
+
 Console.WriteLine("Hello, World!");
 string message1 = "krediler";
 int term = 12;
@@ -38,6 +40,31 @@ string[] loans = { "KREDİ1", "KREDİ2", "KREDİ3", "KREDİ4", "KREDİ5", "KRED�
 for (int i = 0; i < loans.Length;i++)
 {
     Console.WriteLine(loans[i]);
+}
+
+Course course1= new Course();
+course1.Id= 1;
+course1.Name = "c#";
+course1.Description = ".8 ve vs..";
+course1.Price = 0;
+
+Course course2 = new Course();
+course2.Id = 2;
+course2.Name = "java 17...";
+course2.Description = ".8 ve vs..";
+course2.Price =10;
+
+Course course3 = new Course();
+course3.Id = 3;
+course3.Name = "phyton";
+course3.Description = "phyton 3.12...";
+course3.Price =20;
+
+Course[] courses = { course1, course2, course3 };
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
 }
 
 Console.WriteLine("Buton--> Kod Bitti");
